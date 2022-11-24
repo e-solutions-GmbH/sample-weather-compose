074162bb8e7f2eae6fc13b2259cdf26af1e009c4
Christian Schrödel <3532369+ChristianSchroedel@users.noreply.github.com>
2022-11-24 13:56:13 +0100

    refactor(forecast): use constraints for different display sizes
    on-behalf-of: @e-solutions-GmbH <info@esolutions.de>

--- app/src/main/java/de/eso/weather/ui/forecast/ForecastScreen.kt
+++ app/src/main/java/de/eso/weather/ui/forecast/ForecastScreen.kt
@@ -5,17 +5,12 @@ import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.Button
 import androidx.compose.material.SnackbarHost
 import androidx.compose.material.SnackbarHostState
 import androidx.compose.material.Text
@@ -33,7 +28,6 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.layout.layoutId
-import androidx.compose.ui.platform.testTag
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Devices
@@ -41,6 +35,7 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.constraintlayout.compose.ConstraintLayout
 import androidx.constraintlayout.compose.ConstraintSet
+import androidx.constraintlayout.compose.Dimension
 import androidx.navigation.NavController
 import de.eso.weather.R
 import de.eso.weather.domain.forecast.api.WeatherTO
@@ -107,69 +102,103 @@ fun ForecastScreenContent(
 
     val activeLocationName = viewState.activeLocation?.name
 
-    Box {
-        if (WeatherTheme.isLargeScreen()) {
-            Row(modifier = Modifier.fillMaxSize()) {
-                ForecastScreenActiveLocationForecast(
-                    locationHeadlineText = locationHeadlineText,
-                    weatherSummary = weatherSummary,
-                    activeLocationName = activeLocationName,
-                    onGoToWeatherAlertsClicked = onGoToWeatherAlertsClicked,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(3f)
-                        .padding(end = Dimensions.ContainerPadding)
-                )
+    val isLargeScreen = WeatherTheme.isLargeScreen()
 
-                Column(
-                    modifier = Modifier
-                        .weight(2f)
-                        .fillMaxHeight()
-                ) {
-                    ForecastScreenConfigurationPanel(
-                        onShowDummySnackbarClicked = onShowDummySnackbarClicked,
-                        onManageLocationsClicked = onManageLocationsClicked,
-                        onSimulateLocationGoneButton = onSimulateLocationGoneButton
-                    )
+    BoxWithConstraints {
+        val constraints = ConstraintSet {
+            val horizontalSplit = createGuidelineFromStart(0.6f)
 
-                    ForecastScreenEsoLogo(
-                        modifier = Modifier
-                            .fillMaxHeight()
-                            .wrapContentHeight(align = Alignment.Bottom)
-                            .height(Dimensions.IconSizeBigLogo)
-                    )
+            val activeLocationForecastRef = createRefFor("activeLocationForecast")
+            val configurationPanelRef = createRefFor("configurationPanel")
+            val esoLogoRef = createRefFor("esoLogo")
+            val snackbarRef = createRefFor("snackbar")
+
+            if (isLargeScreen) {
+                constrain(activeLocationForecastRef) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(horizontalSplit)
+                    bottom.linkTo(parent.bottom)
+
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+
+                constrain(configurationPanelRef) {
+                    top.linkTo(parent.top)
+                    start.linkTo(horizontalSplit)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(esoLogoRef.top)
+
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+            } else {
+                constrain(activeLocationForecastRef) {
+                    top.linkTo(parent.top)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(configurationPanelRef.top)
+
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
+                }
+
+                constrain(configurationPanelRef) {
+                    top.linkTo(activeLocationForecastRef.bottom)
+                    start.linkTo(parent.start)
+                    end.linkTo(parent.end)
+                    bottom.linkTo(esoLogoRef.top)
+
+                    width = Dimension.fillToConstraints
+                    height = Dimension.fillToConstraints
                 }
             }
-        } else {
-            Column(modifier = Modifier.fillMaxSize()) {
-                ForecastScreenActiveLocationForecast(
-                    locationHeadlineText = locationHeadlineText,
-                    weatherSummary = weatherSummary,
-                    activeLocationName = activeLocationName,
-                    onGoToWeatherAlertsClicked = onGoToWeatherAlertsClicked,
-                    modifier = Modifier
-                        .fillMaxSize()
-                        .weight(1f)
-                        .padding(bottom = Dimensions.ContainerPadding)
-                )
 
-                ForecastScreenConfigurationPanel(
-                    onShowDummySnackbarClicked = onShowDummySnackbarClicked,
-                    onManageLocationsClicked = onManageLocationsClicked,
-                    onSimulateLocationGoneButton = onSimulateLocationGoneButton,
-                    modifier = Modifier.weight(1f)
-                )
+            constrain(esoLogoRef) {
+                top.linkTo(configurationPanelRef.bottom)
+                end.linkTo(parent.end)
+                start.linkTo(configurationPanelRef.start)
+                bottom.linkTo(parent.bottom)
+            }
 
-                ForecastScreenEsoLogo(
-                    modifier = Modifier.height(Dimensions.IconSizeLogo)
-                )
+            constrain(snackbarRef) {
+                bottom.linkTo(parent.bottom)
             }
         }
 
-        SnackbarHost(
-            hostState = snackbarHostState,
-            modifier = Modifier.align(Alignment.BottomCenter)
-        )
+        ConstraintLayout(constraintSet = constraints, modifier = Modifier.fillMaxSize()) {
+            ForecastScreenActiveLocationForecast(
+                locationHeadlineText = locationHeadlineText,
+                weatherSummary = weatherSummary,
+                activeLocationName = activeLocationName,
+                onGoToWeatherAlertsClicked = onGoToWeatherAlertsClicked,
+                modifier = Modifier
+                    .layoutId("activeLocationForecast")
+                    .padding(
+                        end = if (isLargeScreen) Dimensions.ContainerPadding else 0.dp,
+                        bottom = if (isLargeScreen) 0.dp else Dimensions.ContainerPadding
+                    )
+            )
+
+            ForecastScreenConfigurationPanel(
+                onShowDummySnackbarClicked = onShowDummySnackbarClicked,
+                onManageLocationsClicked = onManageLocationsClicked,
+                onSimulateLocationGoneButton = onSimulateLocationGoneButton,
+                modifier = Modifier.layoutId("configurationPanel")
+            )
+
+            ForecastScreenEsoLogo(
+                modifier = Modifier
+                    .layoutId("esoLogo")
+                    .height(Dimensions.IconSizeBigLogo)
+            )
+
+            SnackbarHost(
+                hostState = snackbarHostState,
+                modifier = Modifier.layoutId("snackbar")
+            )
+        }
     }
 }
 
@@ -236,7 +265,7 @@ fun ForecastScreenConfigurationPanel(
             imageVector = Icons.Filled.LocationCity,
             text = stringResource(R.string.manage_locations_button),
             contentDescription = stringResource(R.string.manage_locations_button),
-            modifier = Modifier.padding(bottom = Dimensions.ButtonPadding).fillMaxWidth(),
+            modifier = Modifier.padding(bottom = Dimensions.ButtonPadding),
             textFillsSpace = true
         )
 
@@ -245,7 +274,7 @@ fun ForecastScreenConfigurationPanel(
             imageVector = Icons.Filled.LocationOff,
             text = stringResource(R.string.simulate_location_gone_button),
             contentDescription = stringResource(R.string.simulate_location_gone_button),
-            modifier = Modifier.padding(bottom = Dimensions.ButtonPadding).fillMaxWidth(),
+            modifier = Modifier.padding(bottom = Dimensions.ButtonPadding),
             textFillsSpace = true
         )
 
@@ -254,7 +283,7 @@ fun ForecastScreenConfigurationPanel(
             imageVector = Icons.Filled.Restaurant,
             text = stringResource(R.string.show_dummy_snackbar_button),
             contentDescription = stringResource(R.string.show_dummy_snackbar_button),
-            modifier = Modifier.padding(bottom = Dimensions.ButtonPadding).fillMaxWidth(),
+            modifier = Modifier.padding(bottom = Dimensions.ButtonPadding),
             textFillsSpace = true
         )
     }
@@ -269,111 +298,6 @@ fun ForecastScreenEsoLogo(
         contentDescription = "Eso logo",
         modifier = modifier.fillMaxWidth()
     )
-}
-
-@Composable
-fun ForecastScreenContentDefaultScreenSize(
-    locationHeadlineText: String,
-    weatherSummary: String?,
-    activeLocationName: String?,
-    snackbarHostState: SnackbarHostState,
-    onGoToWeatherAlertsClicked: () -> Unit,
-    onShowDummySnackbarClicked: () -> Unit,
-    onManageLocationsClicked: () -> Unit,
-    onSimulateLocationGoneButton: () -> Unit
-) {
-    BoxWithConstraints {
-        val constraints = ConstraintSet {
-            val locationHeadline = createRefFor("locationHeadline")
-            val weatherSummaryRef = createRefFor("weatherSummary")
-            val alertsButton = createRefFor("alertsButton")
-            val manageLocationsButton = createRefFor("manageLocationsButton")
-            val showDummySbackbarButton = createRefFor("showDummySnackbarButton")
-            val simulateLocationGoneButton = createRefFor("simulateLocationGoneButton")
-            val snackbar = createRefFor("Snackbar")
-
-            constrain(locationHeadline) {
-                top.linkTo(parent.top)
-                start.linkTo(parent.start)
-            }
-            constrain(weatherSummaryRef) {
-                top.linkTo(locationHeadline.bottom)
-                start.linkTo(parent.start)
-            }
-            constrain(alertsButton) {
-                top.linkTo(weatherSummaryRef.bottom, margin = Dimensions.PADDING_MEDIUM)
-                start.linkTo(parent.start)
-            }
-            constrain(manageLocationsButton) {
-                bottom.linkTo(simulateLocationGoneButton.top)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            }
-            constrain(simulateLocationGoneButton) {
-                bottom.linkTo(showDummySbackbarButton.top)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            }
-            constrain(showDummySbackbarButton) {
-                bottom.linkTo(parent.bottom)
-                start.linkTo(parent.start)
-                end.linkTo(parent.end)
-            }
-            constrain(snackbar) {
-                bottom.linkTo(parent.bottom)
-            }
-        }
-        ConstraintLayout(constraints, modifier = Modifier.fillMaxSize()) {
-            weatherSummary?.let {
-                Text(
-                    text = it,
-                    modifier = Modifier.layoutId("weatherSummary")
-                )
-            }
-
-            Text(
-                text = locationHeadlineText,
-                modifier = Modifier.layoutId("locationHeadline")
-            )
-
-            activeLocationName?.let {
-                Button(
-                    modifier = Modifier.layoutId("alertsButton"),
-                    onClick = { onGoToWeatherAlertsClicked() }
-                ) {
-                    Text(text = "Alerts for $it")
-                }
-            }
-
-            Button(
-                modifier = Modifier.layoutId("showDummySnackbarButton"),
-                onClick = onShowDummySnackbarClicked
-            ) {
-                Text(text = stringResource(R.string.show_dummy_snackbar_button))
-            }
-
-            Button(
-                modifier = Modifier.layoutId("manageLocationsButton"),
-                onClick = onManageLocationsClicked
-            ) {
-                Text(text = stringResource(R.string.manage_locations_button))
-            }
-
-            Button(
-                modifier = Modifier
-                    .layoutId("simulateLocationGoneButton")
-                    .testTag("simulateLocationGoneButton"),
-                onClick = onSimulateLocationGoneButton
-            ) {
-                Text(text = stringResource(R.string.simulate_location_gone_button))
-            }
-
-            SnackbarHost(
-                hostState = snackbarHostState,
-                modifier = Modifier.layoutId("Snackbar")
-            )
-        }
-    }
 }
 
 @Preview(device = Devices.AUTOMOTIVE_1024p)
