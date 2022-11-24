9796ddba5199e40e1b996b2d6a0e3977bbcd29cc
Christian Schrödel <3532369+ChristianSchroedel@users.noreply.github.com>
2022-11-24 13:56:08 +0100

    refactor(forecast): extract recurring icon & text composable
    on-behalf-of: @e-solutions-GmbH <info@esolutions.de>

---
+++ app/src/main/java/de/eso/weather/ui/shared/compose/components/IconButton.kt
@@ -0,0 +1,44 @@
+package de.eso.weather.ui.shared.compose.components
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.unit.Dp
+import de.eso.weather.ui.shared.compose.Dimensions
+import de.eso.weather.ui.shared.compose.EsoColors
+
+@Composable
+fun IconAndTextButton(
+    onClick: () -> Unit,
+    imageVector: ImageVector,
+    text: String,
+    contentDescription: String,
+    modifier: Modifier = Modifier,
+    tint: Color = EsoColors.Orange,
+    iconSize: Dp = Dimensions.IconSizeButton,
+    textFillsSpace: Boolean = false
+) {
+    Button(
+        modifier = modifier,
+        onClick = onClick
+    ) {
+        Icon(
+            imageVector = imageVector,
+            contentDescription = contentDescription,
+            tint = tint,
+            modifier = Modifier
+                .padding(end = Dimensions.IconPadding)
+                .size(iconSize)
+        )
+        Text(
+            text = text,
+            modifier = Modifier.weight(1f, textFillsSpace)
+        )
+    }
+}
