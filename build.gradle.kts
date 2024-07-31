c3a0145856852a488b0c3e8f0ec064cdb7745be2
Gabriel Bauer <Gabweb@users.noreply.github.com>
2024-07-31 11:17:29 +0200

    test

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,8 @@
 import org.gradle.api.tasks.testing.logging.TestLogEvent
 
 plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("com.android.library") version "8.2.2" apply false
+    id("com.android.application") version "8.5.1" apply false
+    id("com.android.library") version "8.5.1" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
 }
 
